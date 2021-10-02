057a6fcd62c377e5541d811095e5558b9cf2318c
Alauddin Maulana Hirzan <maulanahirzan@gmail.com>
2021-10-02 15:17:10 +0700

    ProfileActivity

--- app/src/main/java/com/example/praktikum1_mapp/MainActivity.kt
+++ app/src/main/java/com/example/praktikum1_mapp/MainActivity.kt
@@ -5,8 +5,18 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
+import android.widget.Toast
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.ValueEventListener
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var database: DatabaseReference
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -21,18 +31,63 @@ class MainActivity : AppCompatActivity() {
         var btnDaftar = findViewById<Button>(R.id.btnDaftar)
         var btnKeluar = findViewById<Button>(R.id.btnKeluar)
 
+        database = Firebase.database.reference
+
         // Button Action
         btnMasuk.setOnClickListener {
+            val email = editEmail.text.toString()
+            val password = editPassword.text.toString()
+            if(email.equals("")||password.equals(""))
+                Toast.makeText(applicationContext,"Kolom Data Kosong",Toast.LENGTH_SHORT).show()
+            else
+            {
+                val userId = "0"
+                database.child("users")
+                    .child(userId)
+                    .addValueEventListener(object : ValueEventListener {
+                        override fun onDataChange(p0: DataSnapshot) {
+                            val userData = p0.getValue(UserInfo::class.java)
+                            if(userData == null)
+                                Toast.makeText(applicationContext,"Database Kosong",Toast.LENGTH_SHORT).show()
+                            else
+                            {
+                                val resultNama = userData.nama.toString()
+                                val resultEmail = userData.email.toString()
+                                val resultPassword = userData.password.toString()
+
+                                if(email.equals(resultEmail)&&password.equals(resultPassword))
+                                {
+                                    Toast.makeText(applicationContext,"Selamat Datang "+resultNama,Toast.LENGTH_SHORT)
+                                        .show()
+                                    val profile = Intent(applicationContext,ProfileActivity::class.java)
+                                    profile.putExtra("nama",resultNama)
+                                    profile.putExtra("email",resultEmail)
+                                }
+                                else
+                                    Toast.makeText(applicationContext,"Data Login Salah",Toast.LENGTH_SHORT)
+                                        .show()
+                            }
+                        }
+
+                        override fun onCancelled(p0: DatabaseError) {
+                            TODO("Not yet implemented")
+                        }
+
+                    })
+            }
 
         }
+
         btnBersihkan.setOnClickListener {
             editEmail.setText("")
             editPassword.setText("")
         }
+
         btnDaftar.setOnClickListener {
             val formDaftar = Intent(this,FormDaftar::class.java)
             startActivity(formDaftar)
         }
+
         btnKeluar.setOnClickListener {
             finish()
         }
