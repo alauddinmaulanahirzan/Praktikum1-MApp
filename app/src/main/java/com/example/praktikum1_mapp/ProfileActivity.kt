a7a21a53d29e2ad2681b61adbc7c6880ca9bc021
Alauddin Maulana Hirzan <maulanahirzan@gmail.com>
2021-10-16 16:52:10 +0700

    Praktikum 4 - Delete

--- app/src/main/java/com/example/praktikum1_mapp/ProfileActivity.kt
+++ app/src/main/java/com/example/praktikum1_mapp/ProfileActivity.kt
@@ -1,14 +1,24 @@
 package com.example.praktikum1_mapp
 
 import android.app.Activity
+import android.app.AlertDialog
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.InputType
 import android.widget.Button
+import android.widget.EditText
 import android.widget.TextView
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.ValueEventListener
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
 
 class ProfileActivity : AppCompatActivity() {
+
+    private lateinit var database: DatabaseReference
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_profile)
@@ -22,6 +32,8 @@ class ProfileActivity : AppCompatActivity() {
         var btnHapusAkun = findViewById<Button>(R.id.btnHapusAkun)
         var btnKeluar = findViewById<Button>(R.id.btnKeluar)
 
+        database = Firebase.database.reference
+
         txtNama.setText("Nama : "+nama)
         txtEmail.setText("E-Mail : "+email)
 
@@ -32,7 +44,23 @@ class ProfileActivity : AppCompatActivity() {
             startActivityForResult(formUbahData,0)
         }
         btnHapusAkun.setOnClickListener {
+            val buildMessage = AlertDialog.Builder(this@ProfileActivity)
+            val input = EditText(this)
+            input.setHint("Masukkan Password")
+            input.inputType = InputType.TYPE_CLASS_TEXT
+            buildMessage.setView(input)
+            buildMessage.setMessage("Apakah Kamu yakin menghapus "+nama+"?")
+                .setTitle("Peringatan!")
+                .setCancelable(false)
+                .setPositiveButton("Ya") {dialog, id ->
+                    val queryNama = database.child("users")
+                        .child("0")
+                        .orderByChild("email")
+                        .equalTo(email)
 
+                    queryNama.addListenerForSingleValueEvent(object:ValueEventListener{})
+                }
+                .setNegativeButton("Tidak") { dialog, id -> }
         }
         btnKeluar.setOnClickListener {
             finish()
